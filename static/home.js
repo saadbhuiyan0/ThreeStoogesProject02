a040e1a7bca0800d92e7f504bad63f037d6ed7bb
bavrahami00 <bavrahami00@stuy.edu>
2020-01-12 00:27:23 +0000

    Made javascript to add/remove favorites from list

--- static/home.js
+++ static/home.js
@@ -1,20 +1,21 @@
-var changeHeading = function(e) {
-  var h = document.getElementById("h");
-  h.innerHTML = this.innerHTML;
-  //console.log(h.innerHTML);
-};
+var addFav = function(e) {
+  //console.log(e);
+  var n = document.getElementById("favorites");
+  var fav = document.createElement("li");
+  fav.innerHTML = e;
+  fav.addEventListener('click', removeItem);
+  n.append(fav);
+}
 
 var removeItem = function(e) {
+  //console.log(this);
+  addFav(this.innerHTML);
   this.remove();
 }
 
 var lis = document.getElementsByTagName("li");
 
 for (var i = 0; i < lis.length; i++) {
-  //console.log(lis[i]);
-  //console.log("Length: " + lis.length);
-  lis[i].addEventListener('mouseover', changeHeading);
-  lis[i].addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World!";});
   lis[i].addEventListener('click', removeItem);
 }
 
@@ -22,8 +23,6 @@ var addItem = function(e) {
   var list = document.getElementById("thelist");
   var item = document.createElement("li");
   item.innerHTML = "WORD";
-  item.addEventListener('mouseover', changeHeading);
-  item.addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World!";});
   item.addEventListener('click', removeItem);
   list.append(item);
 }
